496cb683be10e072aa56ed983c969e98d5cbb7f1
jane <183669693+Sikzyo@users.noreply.github.com>
2026-08-28 15:43:02 -0500

    feat(menu): show icon

---
+++ internal/menu/logo.go
@@ -0,0 +1,12 @@
+package menu
+
+import "fmt"
+
+func ShowLogo() {
+	fmt.Println("   __ __  __  ___")
+	fmt.Println("  / // / / /_<  /")
+	fmt.Println(" / // /_/ //_/ / ")
+	fmt.Println("/__  __/ ,< / /  ")
+	fmt.Println("  /_/ /_/|_/_/   ")
+	fmt.Println("")
+}
