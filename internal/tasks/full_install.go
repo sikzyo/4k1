c2057dae25738c8c1a09de3d6065f03e37352c69
jane <183669693+Sikzyo@users.noreply.github.com>
2026-09-01 18:39:56 -0500

    feat(install): funcionalidad del script y manejo de errores

--- internal/tasks/full_install.go
+++ internal/tasks/full_install.go
@@ -1,7 +1,23 @@
 package tasks
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/sikzyo/4k1/internal/data"
+	"github.com/sikzyo/4k1/internal/execute"
+)
 
 func FullInstall() {
-	fmt.Println("✦ Iniciando instalación completa :D")
+
+	for _, step := range data.AllSteps {
+		execute.Command("clear")
+		cmd := step.Step()
+
+		if cmd != nil {
+			fmt.Println()
+			fmt.Println("-> Ocurrió un error al momento de ejecutar un proceso, por seguridad el script se va a detener")
+			os.Exit(1)
+		}
+	}
 }
