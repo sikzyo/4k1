06d77cbfc50ed9e8493261f2a13820469bb38ef2
jane <183669693+Sikzyo@users.noreply.github.com>
2026-09-01 13:31:27 -0500

    feat(input): modificar GetInput para manejar errores y retornar valores

--- internal/input/input.go
+++ internal/input/input.go
@@ -1,20 +1,19 @@
 package input
 
 import (
+	"errors"
 	"fmt"
-	"os"
 	"strconv"
 )
 
-func GetInput() int {
+func GetInput() (int, error) {
 	var input string
 	fmt.Println("Ingresa una respuesta:")
 	fmt.Print("-> ")
 	fmt.Scan(&input)
 	number, err := strconv.Atoi(input)
 	if err != nil {
-		fmt.Println("-> Algo salio mal al convertir tu respuesta")
-		os.Exit(1)
+		return -1, errors.New("Solo se aceptan respuestas numéricas")
 	}
-	return number
+	return number, nil
 }
