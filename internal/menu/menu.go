51cd957dd76bb7f7bffb0bb63d45215937784f80
jane <183669693+sikzyo@users.noreply.github.com>
2026-09-24 19:25:08 -0500

    feature(menu): firts version of menu.go

---
+++ internal/menu/menu.go
@@ -0,0 +1,10 @@
+// Package menu utilities to render the menu
+package menu
+
+import "fmt"
+
+func ShowMenu() {
+	fmt.Println("Este es el menu")
+}
+
+// TODO: crear función para renderizar el logo de la app
