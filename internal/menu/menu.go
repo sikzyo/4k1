59ce20a4345b4b48d97be8980ea47ee196cd76db
jane <183669693+sikzyo@users.noreply.github.com>
2026-09-24 19:43:59 -0500

    feat(menu): showIcon function

--- internal/menu/menu.go
+++ internal/menu/menu.go
@@ -7,4 +7,10 @@ func ShowMenu() {
 	fmt.Println("Este es el menu")
 }
 
-// TODO: crear función para renderizar el logo de la app
+func showIcon() {
+	fmt.Println("   __ __  __  ___")
+	fmt.Println("  / // / / /_<  /")
+	fmt.Println(" / // /_/ //_/ / ")
+	fmt.Println("/__  __/ ,< / /  ")
+	fmt.Println("  /_/ /_/|_/_/   ")
+}
